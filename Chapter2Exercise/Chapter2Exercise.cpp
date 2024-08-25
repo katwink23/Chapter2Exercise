dcec480dd555a5a246aa9a3915d71a2291b2d725
Katlyn W. <krw8ateez@gmail.com>
2024-08-25 19:37:21 -0400

    wrote 2.14 code

--- Chapter2Exercise/Chapter2Exercise.cpp
+++ Chapter2Exercise/Chapter2Exercise.cpp
@@ -7,3 +7,19 @@
 // Date: Aug 2024
 // Assignment: IDE and VCS Setup
 // Branch: Master
+
+#include <iostream>
+
+int main()
+ {
+	 char firstInitial;
+	 char middleInitial;
+	 char lastInitial;
+	 std::cout << "Please enter the initial of your first name.";
+	 std::cin >> firstInitial;
+	 std::cout << "Please enter the initial of your middle name.";
+	 std::cin >> middleInitial;
+	 std::cout << "Please enter the initial of your last name.";
+	 std::cin >> lastInitial;
+	 std::cout << "Your initials are: " << firstInitial << middleInitial << lastInitial;
+ }
