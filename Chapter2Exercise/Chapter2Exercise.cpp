e709d63462312059ad2a39f604a29cedd955a3d2
Katlyn W. <krw8ateez@gmail.com>
2024-08-25 19:52:58 -0400

    added 2.17 code

--- Chapter2Exercise/Chapter2Exercise.cpp
+++ Chapter2Exercise/Chapter2Exercise.cpp
@@ -1,5 +1,24 @@
 
 // Name: Katlyn Winkelhake
 // Date: Aug 2024
-// Assignment: IDE and VCS Setup
-// Branch: Master
+// Assignment: Write a program that stores your name, address, and phone number in three separate string
+// objects.
+// Branch: Checkpoint_2.17
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string firstName;
+	std::string homeAddress;
+	std::string phoneNumber;
+	std::cout << "What is your first name?";
+	std::cin >> firstName;
+	std::cout << "What is your home address?";
+	std::cin >> homeAddress;
+	std::cout << "What is your phone number>";
+	std::cin >> phoneNumber;
+
+}
+
